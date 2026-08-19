f0e2f9c6d43ced5e2d1fc6d96d1733799bfc7cd1
Theoclas <fernando.pala.99@gmail.com>
2026-08-19 08:38:40 -0500

    listo area 508408

--- 508408 Guajira copy.js
+++ 508408 Guajira copy.js
@@ -26,7 +26,7 @@ console.log(" Equipo Actual: ", EquipoActual);
 
 const Empresa = "Valleduper";
 const CodigoPin = "V1";
-const ARCHIVO_AREAS = "508393 copy";
+const ARCHIVO_AREAS = "508408 copy";
 const DASHBOARD_URL = "https://annamineria.anm.gov.co/sigm/index.html#/extDashboard";
 const ESPERA_DASHBOARD_MS = 3000;
 const MAX_INTENTOS_DASHBOARD = 3;
@@ -64,9 +64,9 @@ const Datos_Contadores = Contadores[Empresa];
 
 const user1 = Datos_Empresa.Codigo;
 const pass1 = Datos_Empresa.Contraseña;
-const user2 = '';
-const pass2 = '';
-const Agente = 0;
+const user2 = '83401';
+const pass2 = 'G64J1R4M4XR3S2026$';
+const Agente = 1;
 const manual = 0; // 1 = pausa en PIN tras colocarlo; 0 = flujo automático
 const continuarManual = 0; // 1 = el bot solo coloca datos; el humano hace clic en Continuar; 0 = bot también da Continuar
 const continuarAreaManual = 1; // 1 = el humano da Continuar después de colocar el área; 0 = clic automático
--- 508408 Guajira.js
+++ 508408 Guajira.js
@@ -26,7 +26,7 @@ console.log(" Equipo Actual: ", EquipoActual);
 
 const Empresa = "Valleduper";
 const CodigoPin = "V1";
-const ARCHIVO_AREAS = "508393";
+const ARCHIVO_AREAS = "508408";
 const DASHBOARD_URL = "https://annamineria.anm.gov.co/sigm/index.html#/extDashboard";
 const ESPERA_DASHBOARD_MS = 3000;
 const MAX_INTENTOS_DASHBOARD = 3;
@@ -64,8 +64,8 @@ const Datos_Contadores = Contadores[Empresa];
 
 const user1 = Datos_Empresa.Codigo;
 const pass1 = Datos_Empresa.Contraseña;
-const user2 = '97295';
-const pass2 = 'FR3P3RTM2026$';
+const user2 = '83401';
+const pass2 = 'G64J1R4M4XR3S2026$';
 const Agente = 1;
 const manual = 0; // 1 = pausa en PIN tras colocarlo; 0 = flujo automático
 const continuarManual = 0; // 1 = el bot solo coloca datos; el humano hace clic en Continuar; 0 = bot también da Continuar
